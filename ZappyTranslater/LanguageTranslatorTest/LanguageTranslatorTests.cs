9b533cd16317f84238d5a189dbe636111f8a30bd
Rupali <zappyteam@rupalivm>
2020-05-13 11:35:55 +0530

    Added language translator test

---
+++ ZappyTranslater/LanguageTranslatorTest/LanguageTranslatorTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZappyLanguageTranslator;
+
+namespace LanguageTranslatorTest
+{
+    [TestClass]
+    public class LanguageTranslatorTests
+    {
+        [TestMethod]
+        public void Invoke()
+        {
+            //Arrange
+         //   string FilePath = @"C:\...\Documents\ZappySamples\ExtractRequiredValuesFromPDF\PDFExtractionAutomation.docx";
+            string FilePath = "TestFiles/PDFExtractionAutomation.docx";
+            string FromLanguage = "en";
+            string ToLanguage = "ja";
+
+            //Replace your Azure's ResourceKey
+            string TextTranslatorResorceKey = "Replace ResourceKey";
+            LanguageTranslator action = new LanguageTranslator();
+
+            //Act
+            action.FilePath = FilePath;
+            action.FromLanguage = FromLanguage;
+            action.ToLanguage = ToLanguage;
+            action.TextTranslatorResorceKey = TextTranslatorResorceKey;
+            action.Invoke(null, null);
+
+        }
+    }
+}
---
+++ ZappyTranslater/LanguageTranslatorTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LanguageTranslatorTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LanguageTranslatorTest")]
+[assembly: AssemblyCopyright("Copyright © ZappyAI 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("855f12f3-1ce1-4fd6-85e1-27170383d03b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
